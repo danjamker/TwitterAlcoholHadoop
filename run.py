474ce150e1f5b939554526791224827b17ee7d36
Daniel Kershaw <danjamker@gmail.com>
2014-01-27 09:23:44 +0000

    Remove comment block from run

--- run.py
+++ run.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     db = client['AlcoholTweetsResults']
     results = db.results
     print results
-    # mr_job = AlcoholScore(args=['./testdata/TWEETDUMPS.json','-r','emr','--conf-path', '.mrjob.conf'])
-    # with mr_job.make_runner() as runner:
-    #     print  mr_job.show_steps()
-    #     runner.run()
-    #
-    #     for line in runner.stream_output():
-    #         key, value = mr_job.parse_output_line(line)
-    #         value_id = results.insert(value)
-    #         print value
+    mr_job = AlcoholScore(args=['./testdata/TWEETDUMPS.json','-r','emr','--conf-path', '.mrjob.conf'])
+    with mr_job.make_runner() as runner:
+        print  mr_job.show_steps()
+        runner.run()
+
+        for line in runner.stream_output():
+            key, value = mr_job.parse_output_line(line)
+            value_id = results.insert(value)
+            print value
